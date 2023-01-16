83c45f5716e31a384dd42a17cd6e98a830769592
Manuthor <manu.coste@gmail.com>
2023-01-16 10:16:19 +0100

    fix: download native libs via python script

--- scripts/get_native_libraries.py
+++ scripts/get_native_libraries.py
@@ -21,32 +21,36 @@ def download_native_libraries(name: str, version: str, destination: str):
         )
 
         url = f'https://package.cosmian.com/{name}/{version}/all.zip'
-        r = urllib.request.urlopen(url)
-        if r.getcode() != 200:
-            download_native_libraries(name, 'last_build', destination)
-        else:
-            if path.exists('tmp'):
-                shutil.rmtree('tmp')
-            if path.exists('all.zip'):
-                remove('all.zip')
+        try:
+            r = urllib.request.urlopen(url)
+            if r.getcode() != 200:
+                download_native_libraries(name, 'last_build', destination)
+            else:
+                if path.exists('tmp'):
+                    shutil.rmtree('tmp')
+                if path.exists('all.zip'):
+                    remove('all.zip')
 
-            open('all.zip', 'wb').write(r.read())
-            with zipfile.ZipFile('all.zip', 'r') as zip_ref:
-                zip_ref.extractall('tmp')
-                shutil.copyfile(
-                    f'tmp/x86_64-apple-darwin/x86_64-apple-darwin/release/libcosmian_{name}.dylib',
-                    f'{mac}',
-                )
-                shutil.copyfile(
-                    f'tmp/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/release/libcosmian_{name}.so',
-                    f'{linux}',
-                )
-                shutil.copyfile(
-                    f'tmp/x86_64-pc-windows-gnu/x86_64-pc-windows-gnu/release/cosmian_{name}.dll',
-                    f'{windows}',
-                )
-                shutil.rmtree('tmp')
-            remove('all.zip')
+                open('all.zip', 'wb').write(r.read())
+                with zipfile.ZipFile('all.zip', 'r') as zip_ref:
+                    zip_ref.extractall('tmp')
+                    shutil.copyfile(
+                        f'tmp/x86_64-apple-darwin/x86_64-apple-darwin/release/libcosmian_{name}.dylib',
+                        f'{mac}',
+                    )
+                    shutil.copyfile(
+                        f'tmp/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu/release/libcosmian_{name}.so',
+                        f'{linux}',
+                    )
+                    shutil.copyfile(
+                        f'tmp/x86_64-pc-windows-gnu/x86_64-pc-windows-gnu/release/cosmian_{name}.dll',
+                        f'{windows}',
+                    )
+                    shutil.rmtree('tmp')
+                remove('all.zip')
+        except Exception as e:
+            print(f'Cannot get {name} {version} ({e})')
+            download_native_libraries(name, 'last_build', destination)
 
 
 if __name__ == '__main__':
